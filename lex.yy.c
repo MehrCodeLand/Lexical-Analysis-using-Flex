611101c5f323282c312f7ef0ce1053341ca6bd76
Mehrshad <microsoftmehr@gmail.com>
2024-10-24 16:34:11 +0330

    add lexical_2 and 3

--- lex.yy.c
+++ lex.yy.c
@@ -282,11 +282,11 @@ static void yy_fatal_error YY_PROTO(( yyconst char msg[] ));
 	*yy_cp = '\0'; \
 	yy_c_buf_p = yy_cp;
 
-#define YY_NUM_RULES 4
-#define YY_END_OF_BUFFER 5
-static yyconst short int yy_accept[10] =
+#define YY_NUM_RULES 3
+#define YY_END_OF_BUFFER 4
+static yyconst short int yy_accept[8] =
     {   0,
-        0,    0,    5,    2,    3,    1,    2,    1,    0
+        0,    0,    4,    2,    3,    1,    0
     } ;
 
 static yyconst int yy_ec[256] =
@@ -295,16 +295,16 @@ static yyconst int yy_ec[256] =
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    3,    3,    3,
-        3,    3,    3,    3,    3,    3,    3,    1,    1,    1,
-        1,    1,    1,    1,    4,    4,    4,    4,    4,    4,
-        4,    4,    4,    4,    4,    4,    4,    4,    4,    4,
-        4,    4,    4,    4,    4,    4,    4,    4,    4,    4,
-        1,    1,    1,    1,    4,    1,    4,    4,    4,    4,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    3,    3,    3,    3,
 
-        4,    4,    4,    4,    4,    4,    4,    4,    4,    4,
-        4,    4,    4,    4,    4,    4,    4,    4,    4,    4,
-        4,    4,    1,    1,    1,    1,    1,    1,    1,    1,
+        3,    3,    3,    3,    3,    3,    3,    3,    3,    3,
+        3,    3,    3,    3,    3,    3,    3,    3,    3,    3,
+        3,    3,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -321,33 +321,29 @@ static yyconst int yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static yyconst int yy_meta[5] =
+static yyconst int yy_meta[4] =
     {   0,
-        1,    2,    1,    1
+        1,    1,    1
     } ;
 
-static yyconst short int yy_base[12] =
+static yyconst short int yy_base[8] =
     {   0,
-        0,    0,    9,    0,   10,    7,    0,    6,   10,    5,
-        4
+        0,    0,    4,    5,    5,    5,    5
     } ;
 
-static yyconst short int yy_def[12] =
+static yyconst short int yy_def[8] =
     {   0,
-        9,    1,    9,   10,    9,   11,   10,   11,    0,    9,
-        9
+        7,    1,    7,    7,    7,    7,    0
     } ;
 
-static yyconst short int yy_nxt[15] =
+static yyconst short int yy_nxt[9] =
     {   0,
-        4,    5,    4,    6,    8,    7,    7,    7,    9,    3,
-        9,    9,    9,    9
+        4,    5,    6,    7,    3,    7,    7,    7
     } ;
 
-static yyconst short int yy_chk[15] =
+static yyconst short int yy_chk[9] =
     {   0,
-        1,    1,    1,    1,   11,   10,    8,    6,    3,    9,
-        9,    9,    9,    9
+        1,    1,    1,    3,    7,    7,    7,    7
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -361,12 +357,12 @@ static char *yy_last_accepting_cpos;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 "lexical_1.l"
+#line 1 "lexical_3.l"
 #define INITIAL 0
-#line 2 "lexical_1.l"
-  /* definations */  
-/* Rule */
-#line 370 "lex.yy.c"
+#line 2 "lexical_3.l"
+    int count = 0;    
+    
+#line 366 "lex.yy.c"
 
 /* Macros after this point can all be overridden by user definitions in
  * section 1.
@@ -517,10 +513,10 @@ YY_DECL
 	register char *yy_cp, *yy_bp;
 	register int yy_act;
 
-#line 8 "lexical_1.l"
+#line 6 "lexical_3.l"
 
 
-#line 524 "lex.yy.c"
+#line 520 "lex.yy.c"
 
 	if ( yy_init )
 		{
@@ -571,13 +567,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 10 )
+				if ( yy_current_state >= 8 )
 					yy_c = yy_meta[(unsigned int) yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 10 );
+		while ( yy_base[yy_current_state] != 5 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -605,25 +601,20 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 10 "lexical_1.l"
-{printf("Valid\n");}
+#line 8 "lexical_3.l"
+{printf("%s is lowercase character\n", yytext); ++count;}
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
-#line 11 "lexical_1.l"
-{printf("Invalid\n");}
+#line 9 "lexical_3.l"
+{printf("%s is not lowercase character \n" , yytext);}
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 12 "lexical_1.l"
-{return 0;}
-	YY_BREAK
-case 4:
-YY_RULE_SETUP
-#line 14 "lexical_1.l"
+#line 11 "lexical_3.l"
 ECHO;
 	YY_BREAK
-#line 627 "lex.yy.c"
+#line 618 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -915,7 +906,7 @@ static yy_state_type yy_get_previous_state()
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 10 )
+			if ( yy_current_state >= 8 )
 				yy_c = yy_meta[(unsigned int) yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
@@ -950,11 +941,11 @@ yy_state_type yy_current_state;
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 10 )
+		if ( yy_current_state >= 8 )
 			yy_c = yy_meta[(unsigned int) yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
-	yy_is_jam = (yy_current_state == 9);
+	yy_is_jam = (yy_current_state == 7);
 
 	return yy_is_jam ? 0 : yy_current_state;
 	}
@@ -1509,15 +1500,17 @@ int main()
 	return 0;
 	}
 #endif
-#line 14 "lexical_1.l"
-
+#line 11 "lexical_3.l"
 
 
 yywrap() {}
 
+
 int main(){
-    printf("Enter string: ");
+    printf("enter string: ");
     yylex();
 
-    return 0 ;
+    printf("There were %d lowervases character \n" , count);
+
+    return 0;
 }
